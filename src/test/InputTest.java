700700a6aee4bbc7878aeffb422ef409d5d0ef66
han1459 <han1459@cu.ac.kr>
2022-08-09 10:23:31 +0900

    수정된 Employee.java & Input.java

---
+++ src/test/InputTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.example.libs.Employee;
+import com.example.libs.Input;
+
+public class InputTest {
+
+	@Test
+	public void testInput() {
+		Employee [] array = new Employee[1];
+		Input input = new Input(array);
+		int count = input.input();
+//		assertNotNull(array[0]);
+//		assertEquals(1, count);
+		assertEquals("A512", array[0].getEmpno());
+	}
+
+}
